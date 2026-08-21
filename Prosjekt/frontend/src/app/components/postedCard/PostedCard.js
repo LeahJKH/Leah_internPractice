1b9508d761be2f5d5141eb50084d756a7d5d8a27
LeahJKH <leahjb06.kodehode@gmail.com>
2026-08-21 14:45:58 +0200

    minimal design

--- Prosjekt/frontend/src/app/components/postedCard/PostedCard.js
+++ Prosjekt/frontend/src/app/components/postedCard/PostedCard.js
@@ -10,17 +10,19 @@ info = info.info
   case 3: col = "blue"  
 }
     return(
-    <div className="PostedCard">
-        <figure>
-
-        {info.image? <Image src={info.image} alt="Image Yipeee" width={100} height={100}/>: <Image src="/tempImg" alt="no image" width={100} height={100} />} {/* note too self fix alt tags and solve it. iykyk */}
+    <div className={style.PostedCard}>
+        <figure className={style.imgCont}>
+            {info.image? <Image src={info.image} alt="Image Yipeee" width={300} height={124}/>: <Image src="/tempImg.webp" alt="no image" width={300} height={124} />} {/* note too self fix alt tags and solve it. iykyk */}
         </figure>
+
         <div className={style.seperator} style={{backgroundColor: col}}></div>
-        <aside>
-        <h3>{info.tittel}</h3>
-        <p>{info.description}</p>
+
+        <aside className={style.leftBot}>
+            <h3>{info.tittel}</h3>
+            <p>{info.description}</p>
         </aside>
-        <aside>
+
+        <aside className={style.rightBot}>
             <p>{info.time}</p>
             <p>{info.date}</p>
             <button>Se mer</button>
