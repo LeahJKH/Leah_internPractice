098d52a80669038f360262abb5d3ee0d79768db7
LeahJKH <leahjb06.kodehode@gmail.com>
2026-08-21 13:12:58 +0200

    made cards actually post

--- Prosjekt/frontend/src/app/UserDash/page.js
+++ Prosjekt/frontend/src/app/UserDash/page.js
@@ -1,6 +1,25 @@
+'use client'
+import PostedCard from "@/app/components/postedCard/PostedCard.js"
+import {useEffect, useState} from 'react'
 export default function UserDash() {
+    const [arrengement, setarrengement] = useState([]);
+
+useEffect(()=> {
+    fetch("/TestData/planboard.json")
+    .then(res => res.json())
+    .then(data => setarrengement(data))
+},[]) 
+
     return(
         <>
+        <main>
+            <section>
+                {arrengement.map((e) => {
+                    return(
+                     <PostedCard key={e.id} info={e} />
+                        )})}
+            </section>
+        </main>
         </>
     )
 }
--- Prosjekt/frontend/src/app/components/postedCard/PostedCard.js
+++ Prosjekt/frontend/src/app/components/postedCard/PostedCard.js
@@ -1,18 +1,21 @@
 import Image from 'next/image'
+import style from './PostedCard.module.css'
+
 export default function Postedcard(info) {
-let col;
+info = info.info
+    let col;
     switch(info.type) {
   case 1: col = "green";
   case 2: col = "red";
-  case 3: col = "pink"  
+  case 3: col = "blue"  
 }
     return(
     <div className="PostedCard">
         <figure>
 
-        {info.image? <Image src={info.image} alt="Image Yipeee"/>: <Image src="/tempImg" alt="no image" />} {/* note too self fix alt tags and solve it. iykyk */}
+        {info.image? <Image src={info.image} alt="Image Yipeee" width={100} height={100}/>: <Image src="/tempImg" alt="no image" width={100} height={100} />} {/* note too self fix alt tags and solve it. iykyk */}
         </figure>
-        <hr  style={`backgroundColor: ${col}`}></hr>
+        <div className={style.seperator} style={{backgroundColor: col}}></div>
         <aside>
         <h3>{info.tittel}</h3>
         <p>{info.description}</p>
