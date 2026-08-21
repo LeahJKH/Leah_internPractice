183e47dd6a680b42e6ee3cc02e4e334737c1a423
LeahJKH <leahjb06.kodehode@gmail.com>
2026-08-21 09:44:20 +0200

    frontend startup

---
+++ Prosjekt/frontend/src/app/AdminDash/page.js
@@ -0,0 +1,6 @@
+export default function AdminDash() {
+    return(
+        <>
+        </>
+    )
+}
---
+++ Prosjekt/frontend/src/app/UserDash/SettingsPage/page.js
@@ -0,0 +1,6 @@
+export default function SettingsPageU() {
+    return(
+        <>
+        </>
+    )
+}
