df868bc265daaf434b5a273940aabcea4406fcfe
LeahJKH <leahjb06.kodehode@gmail.com>
2026-08-21 15:19:08 +0200

    removed some pages for lower load

--- Prosjekt/frontend/src/app/AdminDash/SettingsPage/page.js
+++
@@ -1,6 +0,0 @@
-export default function SettingsPageA() {
-    return(
-        <>
-        </>
-    )
-}
--- Prosjekt/frontend/src/app/AdminDash/page.js
+++
@@ -1,6 +0,0 @@
-export default function AdminDash() {
-    return(
-        <>
-        </>
-    )
-}
